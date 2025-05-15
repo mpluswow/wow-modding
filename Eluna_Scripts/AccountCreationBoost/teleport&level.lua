4e69aedc44411f95bc415d819557a03f484ca7f1
Ghost <48277056+mpluswow@users.noreply.github.com>
2025-05-15 12:10:14 -0700

    Updates

--- Eluna_Scripts/AccountCreationBoost/teleport&level.lua
+++ Eluna_Scripts/AccountCreationBoost/teleport&level.lua
@@ -3,28 +3,29 @@ local TELEPORT_X = 5940.51
 local TELEPORT_Y = 623.757
 local TELEPORT_Z = 650.654
 local TELEPORT_O = 2.73016     -- orientation
-local TARGET_LEVEL = 70        -- desired level
+local TARGET_LEVEL = 70
+local REWARD_GOLD = 250000     -- 2500 gold in copper
 
--- Hook into OnLogin
 function OnLogin(event, player)
-    if player:GetLevel() == 1 and player:GetTotalPlayedTime() < 60 then
-        player:SendBroadcastMessage("Welcome! Teleporting in 5 seconds...")
-
+    if player:GetTotalPlayedTime() < 60 then
         local guid = player:GetGUIDLow()
+        
+        player:SetLevel(TARGET_LEVEL)
+        player:SendBroadcastMessage("Welcome! You have been leveled to " .. TARGET_LEVEL .. ". Teleporting in 5 seconds...")
 
-        -- Delay 5 seconds for teleport
+        -- Wait 5 seconds, then teleport
         CreateLuaEvent(function()
             local p = GetPlayerByGUID(guid)
             if p and p:IsInWorld() then
                 p:Teleport(TELEPORT_MAP, TELEPORT_X, TELEPORT_Y, TELEPORT_Z, TELEPORT_O)
-                p:SendBroadcastMessage("Teleported! Leveling up in 5 seconds...")
+                p:SendBroadcastMessage("Teleported! You'll receive your gold in 5 seconds...")
 
-                -- Delay another 5 seconds for level-up
+                -- Wait another 5 seconds, then reward gold
                 CreateLuaEvent(function()
                     local p2 = GetPlayerByGUID(guid)
                     if p2 and p2:IsInWorld() then
-                        p2:SetLevel(TARGET_LEVEL)
-                        p2:SendBroadcastMessage("Leveled up to " .. TARGET_LEVEL .. "!")
+                        p2:ModifyMoney(REWARD_GOLD)
+                        p2:SendBroadcastMessage("You've received 2500 gold. Enjoy your adventure!")
                     end
                 end, 5000, 1)
             end
